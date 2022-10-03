6d2eef32e29003fe771dc0f2fcb25c75cd7a9841
InnaShtondenko <inna.mycase@gmail.com>
2022-10-04 02:23:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,54 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './fetchCountries';
-import debounce from "lodash.debounce";
 
+import ImagesApiService from './fetchImagesAPI';
+import appendImagesMarkup from './markup';
 
 const refs = {
-    input: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info'),
+    seachForm: document.querySelector('#search-form'),
+    galleryContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more'),
 };
 
-const DEBOUNCE_DELAY = 300;
 
-refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+refs.seachForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onInput(e) {
-    e.preventDefault();
+let searchValue = '';
+let pageNumber = 1;
+refs.loadMoreBtn.hidden = true;
 
-    const countryName = refs.input.value.trim();
-    if (!countryName) {
-        refs.countryList.innerHTML = '';
-        refs.countryInfo.innerHTML = '';
-        return;
-    }
+function onSearch(e) {
+  e.preventDefault();
+  refs.loadMoreBtn.hidden = false;
 
-    fetchCountries(countryName)
-        .then(renderCountries)
-        .catch(onError)
+  galleryContainer();
+  searchValue = e.currentTarget.elements.searchQuery.value;
+
+  if (!searchValue) {
+    refs.loadMoreBtn.hidden = true;
+    return Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  pageNumber = 1;
+  ImagesApiService(searchValue, pageNumber).then(appendImagesMarkup);
 }
 
-function renderCountries(countries) {
-    refs.countryList.innerHTML = '';
-    refs.countryInfo.innerHTML = '';
-    if (countries.length > 10) {
-        Notify.info("Too many matches found. Please enter a more specific name.")
-    }
-
-    if (countries.length > 2 && countries.length < 10) {
-        const list = countries.map(({ flags, name }) => {
-            return `<li class="county-list__item">
-            <img class="image" src="${flags.svg}" alt="" width="40" height="40"
-            <h2 class="country-list__title">${name.official}</h2>
-            </li>`
-        }).join('');
-        refs.countryList.innerHTML = list;
-    }
-
-    if (countries.length === 1) {
-        const markup = countries.map(({ flags, name, capital, population, languages }) => {
-            return `<img src="${flags.svg}" alt="${name.official}" width="50" height="40"
-            <h2 class="official">${name.official}</h2>
-            <div class="discription">
-            <p><span>Capital:</span>${capital}</p>
-            <p><span>Population:</span>${population}</p>
-            <p><span>Languages:</span>${Object.values(languages)}</Object></p>
-            </div>`
-        }).join('');
-        refs.countryInfo.innerHTML = markup;
-    }
+function onLoadMore() {
+  pageNumber += 1;
+  ImagesApiService(searchValue, pageNumber)
+    .then(appendImagesMarkup)
+    .catch(() => {
+      refs.loadMoreBtn.hidden = true;
+      Notify.info(
+        `We're sorry, but you've reached the end of search results.`
+      );
+    });
 }
 
-function onError() {
-    Notify.failure("Oops, there is no country with that name");
+
+function galleryContainer() {
+  galleryContainer.innerHTML = '';
 }
